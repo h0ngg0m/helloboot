de907f68caca05d05988f3f60c73af82f2659f41
h0ngg0m <jaehongbyeon98@gmail.com>
2024-11-20 22:23:59 +0900

    :recycle: 어노테이션으로 컨트롤러가 매핑될 수 있게 적용

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,13 @@
 package tobyspring.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Objects;
 
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
 
     private final HelloService helloService;
@@ -10,6 +16,7 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping
     public String hello(final String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
