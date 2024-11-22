0e9da8025eb170387fbc70ac7e011d17d45e9590
h0ngg0m <jaehongbyeon98@gmail.com>
2024-11-22 16:45:01 +0900

    :sparkles: application.properties 값 출력

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,10 +1,21 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import tobyspring.config.MySpringBootApplication;
 
 @MySpringBootApplication
 public class HellobootApplication {
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment env) {
+        return args -> {
+            String myName = env.getProperty("my.name");
+            System.out.println("my.name = " + myName);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HellobootApplication.class, args);
