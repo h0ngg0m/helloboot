4220753209adf14552b11540a1fd04dcb5796d23
h0ngg0m <jaehongbyeon98@gmail.com>
2024-11-22 15:45:24 +0900

    :sparkles: true, false condition

---
+++ src/test/java/tobyspring/study/ConditionalTest.java
@@ -0,0 +1,81 @@
+package tobyspring.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConditionalTest {
+
+    @Test
+    void conditional() {
+        new ApplicationContextRunner().withUserConfiguration(Config1.class)
+                .run(context -> {
+                    assertThat(context).hasSingleBean(MyBean.class);
+                    assertThat(context).hasSingleBean(Config1.class);
+                });
+
+        new ApplicationContextRunner().withUserConfiguration(Config2.class)
+                .run(context -> {
+                    assertThat(context).doesNotHaveBean(MyBean.class);
+                    assertThat(context).doesNotHaveBean(Config2.class);
+                });
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @Conditional(TrueCondition.class)
+    @interface TrueConditional {
+
+    }
+
+    @Configuration
+    @TrueConditional
+    static class Config1 {
+        @Bean
+        MyBean myBean() {
+            return new MyBean();
+        }
+    }
+
+    static class MyBean {
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @Conditional(FalseCondition.class)
+    @interface FalseConditional {
+
+    }
+
+    @Configuration
+    @FalseConditional
+    static class Config2 {
+        @Bean
+        MyBean myBean() {
+            return new MyBean();
+        }
+    }
+
+    static class TrueCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return true;
+        }
+    }
+
+    static class FalseCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
+    }
+
+}
