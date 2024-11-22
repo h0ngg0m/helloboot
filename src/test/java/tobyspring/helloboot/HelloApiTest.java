6d59428f9fdd922234dfeb60d0ed78546bca67ff
h0ngg0m <jaehongbyeon98@gmail.com>
2024-11-22 10:44:06 +0900

    :white_check_mark: HelloApiTest

---
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -0,0 +1,22 @@
+package tobyspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class HelloApiTest {
+
+    @Test
+    void helloApi() {
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> result = rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        Assertions.assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(result.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).contains("text/plain");
+        Assertions.assertThat(result.getBody()).isEqualTo("Hello Spring");
+    }
+}
