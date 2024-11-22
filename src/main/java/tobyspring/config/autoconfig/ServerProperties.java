038c00ba3ab4f4d54794dbc9707d7da12bee0bae
h0ngg0m <jaehongbyeon98@gmail.com>
2024-11-22 17:49:37 +0900

    :recycle: 프로퍼티를 클래스 분리

---
+++ src/main/java/tobyspring/config/autoconfig/ServerProperties.java
@@ -0,0 +1,28 @@
+package tobyspring.config.autoconfig;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class ServerProperties {
+
+    @Value("${contextPath:}")
+    private String contextPath;
+
+    @Value("${port:8080}")
+    private int port;
+
+    public String getContextPath() {
+        return contextPath;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setContextPath(String contextPath) {
+        this.contextPath = contextPath;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
---
+++ src/main/java/tobyspring/config/autoconfig/ServerPropertiesConfig.java
@@ -0,0 +1,17 @@
+package tobyspring.config.autoconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+public class ServerPropertiesConfig {
+
+    @Bean
+    public ServerProperties serverProperties(Environment environment) {
+        ServerProperties properties = new ServerProperties();
+
+        properties.setContextPath(environment.getProperty("contextPath"));
+        properties.setPort(Integer.parseInt(environment.getProperty("port")));
+
+        return properties;
+    }
+}
